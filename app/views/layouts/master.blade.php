ed9cdf9124de17db2a7a239219fb6718cebef27b
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-02-24 16:58:09 -0600

    Added in the logo and made it so that it shrinks or grows with the screen

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -8,39 +8,46 @@
 </head>
 <body>
 <div class="container-fluid">
-	<div class="row navbar navbar-fixed-top">
-		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-			<a href="{{{action('HomeController@landingpage')}}}"><div class="col-lg-2 col-md-2">
-				Welcome
-			</div></a>
-			<a href="{{{action('HomeController@about')}}}"><div class="col-lg-2 col-md-2">
-				About Ming Dynasty
-			</div></a>
-			<a href="{{{action('HomeController@services')}}}"><div class="col-lg-2 col-md-2">
-				Signature Services
-			</div></a>
-			<a href="{{{action('HomeController@contact')}}}"><div class="col-lg-2 col-md-2">
-				Request Appointment
-			</div></a>
-			<a href="{{{action('HomeController@products')}}}"><div class="col-lg-4 col-md-4">
-				Products and Gift Certificates
-			</div></a>
+	<div class="navbar-fixed-top">
+		<div class="row topnavbar">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+				<img class="logo" src="/img/logo.png">
+			</div>
+		</div>
+		<div class="row navbar">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+				<a href="{{{action('HomeController@landingpage')}}}"><div class="col-lg-2 col-md-2">
+					Welcome
+				</div></a>
+				<a href="{{{action('HomeController@about')}}}"><div class="col-lg-2 col-md-2">
+					About Ming Dynasty
+				</div></a>
+				<a href="{{{action('HomeController@services')}}}"><div class="col-lg-2 col-md-2">
+					Signature Services
+				</div></a>
+				<a href="{{{action('HomeController@contact')}}}"><div class="col-lg-2 col-md-2">
+					Request Appointment
+				</div></a>
+				<a href="{{{action('HomeController@products')}}}"><div class="col-lg-4 col-md-4">
+					Products and Gift Certificates
+				</div></a>
+			</div>
 		</div>
 	</div>
 	<div class="footer navbar-fixed-bottom">
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-				<div class="col-lg-4 col-md-4">
-					Call us to book<br>
+				<strong><div class="col-lg-4 col-md-4">
+					<span class="footline">Call us to book</span></strong><br>
 					1-806-355-5695<br>
 					1-806-676-8252 (text)
 				</div>
 				<div class="col-lg-4 col-md-4">
-					Email us<br>
+					<strong><span class="footline">Email us</span></strong><br>
 					thai@mingdynasty.co
 				</div>
 				<div class="col-lg-4 col-md-4">
-					Find us<br>
+					<strong><span class="footline">Find us</span></strong><br>
 					5221 Floyd Ave<br>
 					Amarillo, TX 79106
 				</div>
@@ -49,7 +56,7 @@
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
 				<div class="col-lg-6 col-lg-offset-3">
-					Copyright © 2016 Ming Dynasty | Site Design by Victor Vo and Reagan Wilkins
+					<span class="disclaimer">Copyright © 2016 Ming Dynasty | Site Design by Victor Vo and Reagan Wilkins</span>
 				</div>
 			</div>
 		</div>

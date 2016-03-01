2d0c47b1aebb73e087e2b0ca75916a58ae5f186f
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-03-01 15:37:43 -0600

    finished pulling in the google maps API and placing the marker.

--- app/views/contact.blade.php
+++ app/views/contact.blade.php
@@ -28,9 +28,9 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2">
+		<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-xs-8">
 			<form class="form-horizontal" method="POST">
-				<div class="col-lg-2 col-md-2">
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
 					<div class="form-group">
 						<input type="text" name="name" placeholder="name">
 					</div>
@@ -45,13 +45,47 @@
 					</div>
 				</div>
 				<div class="form-group">
-					<div class="col-lg-4 col-md-4">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
 						<textarea class="messagebox" cols="20" rows="5" name="message">Message</textarea><br>
 						<button class="btn btn-primary">Submit</button>
 					</div>
 				</div>
 			</form>
 		</div>
+				<div class="maparea col-lg-6 col-lg-offset-6 col-md-6">
+					<div id="mapcanvas"></div>
+				</div>
 	</div>
 </div>
+@stop
+@section('bottom-script')
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeynTTojZWMRs8rdixeaNibGGEgS-MEk"></script>
+	<script type="text/javascript">
+	(function() {
+		"use strict";
+
+		var theposition = {
+			lat: 35.178132,
+			lng: -101.896930
+		}
+
+		var mapOptions = {
+			zoom: 18,
+
+			center: theposition
+		};
+
+		var map = new google.maps.Map(document.getElementById("mapcanvas"), mapOptions);
+
+		var infowindow = new google.maps.InfoWindow({
+			content: "Ming Dynasty (Behind Lifeway Fellowship Church)",
+		});
+		var marker = new google.maps.Marker({
+			position: theposition,
+			map: map,
+			title: "Ming Dynasty"
+		});
+			infowindow.open(map, marker);
+	})();
+	</script>
 @stop
